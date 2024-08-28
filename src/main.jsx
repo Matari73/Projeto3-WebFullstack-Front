f9dcd3629675d6bcd643deb884bda119ed9a8d62
Matari73 <marianadeoliveira73@hotmail.com>
2024-08-28 18:22:12 -0300

    Adicionando react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './pages/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
